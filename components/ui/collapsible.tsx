9a16c5da5d7e3f567dc093e295260220d46e173b
Zheng Li <lizheng@lizheng.me>
2026-09-04 21:35:39 +0800

    refactor(ui): re-export Basalt Avatar, Tooltip, and Collapsible

--- components/ui/collapsible.tsx
+++ components/ui/collapsible.tsx
@@ -1,9 +1,7 @@
-import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
-
-const Collapsible = CollapsiblePrimitive.Root;
-
-const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
-
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
-
-export { Collapsible, CollapsibleContent, CollapsibleTrigger };
+export {
+  Collapsible,
+  CollapsibleContent,
+  type CollapsibleContentProps,
+  type CollapsibleProps,
+  CollapsibleTrigger,
+} from "@nocoo/basalt/components/collapsible";
